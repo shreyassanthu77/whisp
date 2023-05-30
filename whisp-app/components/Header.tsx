a6a5794ae337880fe99f30f6a1f0b010288682e3
Shreyas Mididoddi <shreyassanthu77@gmail.com>
2023-05-30 05:36:23 +0000

    feat: add fe

---
+++ whisp-app/components/Header.tsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header>
+        <div className="container mx-auto">
+            <strong>
+                Whisp
+            </strong>
+        </div>
+    </header>
+  )
+}
+
+export default Header
